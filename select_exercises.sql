7741d7f96e08b02150195652310f611a9c369dbf
Nicholas Sosa <nicksosa456@gmail.com>
2019-08-14 11:28:11 -0500

    added captions, fixed error in seeder

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,5 +1,13 @@
 USE codeup_test_db;
+select 'all albums by Pink Floyd' as '';
 select album_name from albums where artist_name = 'Pink Floyd';
+select 'release year for Sgt. Pepper''s Lonely Hearts Club' as '';
 select release_date from albums where album_name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+select 'genre for Nevermind album' as '';
 select genre from albums where album_name = 'Nevermind';
--- select album_name from albums where release_date = between 1990 and 1999;
+select 'albums released in the 1990s' as '';
+select album_name from albums where release_date between 1990 and 1999;
+select 'albums with less than 20 million certified sales' as '';
+select album_name from albums where record_sales < 20.0;
+select 'albums with rock genre' as '';
+select album_name from albums where genre = 'rock';
